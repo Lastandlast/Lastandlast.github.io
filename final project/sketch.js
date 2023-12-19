2b89a8cabc7a5e5e4bef96b84cde4fc378c810ab
Lastandlast <sy3405046211@gmail.com>
2023-12-19 12:19:11 -0600

    add hook

--- final project/sketch.js
+++ final project/sketch.js
@@ -4,27 +4,43 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let img;
+let ocean;
+let submarine;
+let hookImage;
+let hook;
 function preload(){
-  img = loadImage('assets/oceanjpg.jpg')
-
+  ocean = loadImage('assets/oceanjpg.jpg');
+  submarine = loadImage('assets/submarine.png');
+  hookImage = loadImage('assets/hook.png');
+  
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  hook = new Hook(100, 100);
 }
 
 function draw() {
   background(255);
-  image(img, 0, 0,windowWidth, windowHeight);
+  image(ocean, 0, 0,windowWidth, windowHeight);
+  image(submarine, 800, -25, 300, 250);
+  hook.display();
 }
 
 
-class game{
-  constructor (){
-    this.canvas = null;
-    this.context = null;
-    this.score = 0;
-    this.init();
+class Hook{
+  constructor (x, y){
+    this.position = createVector(x, y);
+    this.target = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.state = 0;
+    
   }
+  display (){
+    image(hookImage,this.position.x,this.position.y);
+    
+  }
+
+  
 }
+
