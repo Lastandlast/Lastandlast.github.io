b0a11a4fe78f68d96b683811f98e95a6337d9ef2
Lastandlast <sy3405046211@gmail.com>
2023-12-21 12:17:11 -0600

    12212023

--- final project/sketch.js
+++ final project/sketch.js
@@ -1,47 +1,55 @@
-// Project Title  
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 let ocean;
 let submarine;
 let hookImage;
 let hook;
-function preload(){
+let subX = 950;
+let subY = 150;
+
+function preload() {
   ocean = loadImage('assets/oceanjpg.jpg');
   submarine = loadImage('assets/submarine.png');
   hookImage = loadImage('assets/hook.png');
-  
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  hook = new Hook(100, 100);
+  hook = new Hook(950, 250);
 }
 
 function draw() {
   background(255);
-  image(ocean, 0, 0,windowWidth, windowHeight);
+  image(ocean, 0, 0, windowWidth, windowHeight);
   image(submarine, 800, -25, 300, 250);
+  hook.move();
   hook.display();
 }
 
-
-class Hook{
-  constructor (x, y){
+class Hook {
+  constructor(x, y) {
     this.position = createVector(x, y);
     this.target = createVector(x, y);
     this.vel = createVector(0, 0);
     this.state = 0;
-    this.size = ;
+    this.size = 50;
+    this.angle = 0;
   }
-  display (){
-    image(hookImage,this.position. x,this.position.y);
-    
-  }
-  
 
-  
+  move() {
+    
+    this.angle = radians(map(sin(frameCount * 0.02), -1, 1, 0, 180));
+    this.position.x = subX + cos(this.angle) * 100; 
+    this.position.y = subY + sin(this.angle) * 100; 
+  }
+
+  display() {
+    line(subX, subY, this.position.x, this.position.y);
+    imageMode(CENTER);
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.angle + radians(270));
+    image(hookImage, 0, 0, this.size, this.size);
+    
+    imageMode(CORNER);
+  }
 }
 
